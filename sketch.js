19e6ee62e36272112ab9d9d0e7b225d403ac86cc
Robinshi0520 <xshi0520@uni.sydney.edu.au>
2025-10-05 16:51:39 +0800

    update NEW MAP ADDED

--- sketch.js
+++ sketch.js
@@ -3,6 +3,8 @@ let unlockedAchievements = [];
 
 let teleportLock = false; // 防抖，避免同帧/连续传送
 let firstTeleportUnlocked = false; // 全局变量
+let firstBesideE = false; // 全局标记，只触发一次
+let overlayUntil = 0; // 毫秒时间戳：在此时间前显示遮盖
 
 
 let maps = [];
@@ -42,12 +44,25 @@ let treeImg;
 let bushImg;
 let fenceImg;
 let fenceImg1;
-let portalImg;
+let chicImg;
 let seaImg;
 let seaImg1;
 let beachImg;
 let beachImg1;
 let shellImg;
+let appleImg;
+let coopImg;
+let wellImg;
+let flowerImg;
+
+let firstpage;
+let stonewalk1;
+let stonewalk2;
+let flowerwalk1;
+let flowerwalk2;
+
+
+
 
 
 function preload() {
@@ -60,12 +75,20 @@ function preload() {
   bushImg = loadImage('assets/bush.gif');
   fenceImg = loadImage('assets/fence.gif');
   fenceImg1 = loadImage('assets/fence1.gif');
-  portalImg = loadImage('assets/portal.gif');
+  chicImg = loadImage('assets/chicken.gif');
   seaImg = loadImage('assets/sea.gif');
   seaImg1 = loadImage('assets/sea1.gif');
   beachImg = loadImage('assets/beach.gif');
   beachImg1 = loadImage('assets/beach1.gif');
   shellImg = loadImage('assets/shell.gif');
+  appleImg = loadImage('assets/apple.gif');
+  coopImg = loadImage('assets/coop.gif');
+  wellImg = loadImage('assets/well.gif');
+  flowerImg = loadImage('assets/flower.gif');
+  firstpage = loadImage('assets/firstpage.gif');
+  stonewalk1 = loadImage('assets/stonewalk1.gif');
+  flowerwalk1 = loadImage('assets/flowerwalk1.gif');
+  flowerwalk2 = loadImage('assets/flowerwalk2.gif');
 
   // ========= 加载每方向 3 帧图 =========
   anim.down[0]  = loadImage('assets/player_down_0.gif');
@@ -94,18 +117,18 @@ function setup() {
   maps[0] = [
     
     "----------N---------",
-    "1BBBBBBBB......TTTT1",
-    "1TTT......TB.B.....1",
-    "1.........TB.......1",
-    "1...B.....T.....T..1",
-    "<..................1",
-    "1............BBBBBB1",
-    "1TTTTTT......TRRRRR1",
-    "1BBBBBBBBB...TRRRRR1",
+    "1BBBBBBBB.@.BBBBBBB1",
+    "1PCC......@.....CTT1",
+    "1.........@B..C....1",
+    "1P.C.B...T@.....TTT1",
+    "<@@@@@@@@@@@@@@@@@@>",
+    "1C........@.CBBBBBB1",
+    "1TW.......@..TRRRRR1",
+    "1BTBBBBBBB@.WTRRRRR1",
     "----------!---------",
   ];
 
-  maps[1] = [//> mountain暂时没有
+  maps[1] = [//> mountain
      "----------N---------",
     "1BBBBB.........TTTT1",
     "1TTT.........B.....1",
@@ -119,16 +142,16 @@ function setup() {
   ];
 
   maps[2] = [//< beach
-    "SSSE***.X&..*..&&...",
-    "SSSE.X..&&&..X......",
-    "SSSE&&..X...&...*...",
-    "SSSEX..*.*...&...*..",
-    "SSSSE&&.&&....*&*...",
+    "SSSE***.X&..*..&&..O",
+    "SSSE.X..&&&..X.....O",
+    "SSSE&&..X...&...*..O",
+    "SSSEX..*.*...&...*.O",
+    "SSSSE&&.&&....*&*..O",
     "SSSSE*&**...&...*&*O",
-    "SSSSSSSSE&*..X......",
-    "SSSSSSSSSSE*&&*X....",
-    "SSSSSSSSSSSSSSE&**..",
-    "SSSSSSSSSSSSSSSSSSE.",
+    "SSSSSSSSE&*..X.....O",
+    "SSSSSSSSSSE*&&*X...O",
+    "SSSSSSSSSSSSSSE&**.O",
+    "SSSSSSSSSSSSSSSSSSEO",
   ];
 
   maps[3] = [//^ forest
@@ -136,12 +159,26 @@ function setup() {
     "####################",
     "#TTTTTTTTTGTTTTTTTT#",
     "#TTTTTT.......TTTTT#",
-    "#BBBBB........BBBBB#",
+    "#BBBBBA.......BBBBB#",
     "#BBBBBBB.....BBBBBB#",
     "#RRRRRRB.....BRRRRR#",
     "#RRRRRRB.....BRRRRR#",
     "#RRRRRRB.....BRRRRR#",
     "##########O#########",
+  ];
+  maps[4] = [//! grasslandmaze
+    "BBBBBBBBBB.BBBBBBBBB",
+    "BDL.L.BLLL.LL.LL#.DB",
+    "B.TTT.B.BBBBBBBBLL.B",
+    "B.TF..BL.LLL.LB.DBBB",
+    "B.TTT..LTTTTT.BD.B.B",
+    "BL..L.B.LL.T.LL..L.B",
+    "BBBBBBBBBB.TTTTTTT.B",
+    "BDD#DDB.L.LBLLDDDBLB",
+    "BDBBBBB.BBBBBBBBBB.B",
+    "BDDDDDD#DDDDDDDDDOOB",
+    
+
   ];
 
    // 初始化像素目标
@@ -167,6 +204,9 @@ function draw() {
   }
   else if (currentMap === 3) {
     background(215, 250, 160); // green
+  }
+  else if (currentMap === 4) {
+    background(70, 229, 23); // green
   }
 
   drawMap(maps[currentMap]);
@@ -229,6 +269,18 @@ function draw() {
 }
   //
   handleContinuousMove();
+  //
+  checkBeside();
+  //
+  if (millis() < overlayUntil && firstpage) {
+    push();
+    imageMode(CENTER);
+                   // 半透明覆盖，可删
+    image(firstpage, width/2, height/2, 220, 220); // 居中 220×220
+    pop();
+  }
+
+  
 }
 
 function drawMap(map) {
@@ -241,10 +293,7 @@ function drawMap(map) {
       } else if (ch === "N") {
        imageMode(CENTER);
        image(npc1Img, x * tileSize + tileSize / 2, y * tileSize + tileSize / 2, tileSize, tileSize);
-      } else if (ch === ">" || ch === "<"|| ch === "^"|| ch === "!"|| ch === "O") {
-        imageMode(CENTER);
-        image(portalImg, x * tileSize + tileSize / 2, y * tileSize + tileSize / 2, tileSize, tileSize);
-      }
+      } 
        else if (ch === "#") {
         imageMode(CENTER);
         image(treeImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
@@ -284,14 +333,48 @@ function drawMap(map) {
         imageMode(CENTER);
         image(npc1Img, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
       } 
+      else if (ch === "A") {
+        imageMode(CENTER);
+        image(appleImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+      else if (ch === "C") {
+        imageMode(CENTER);
+        image(chicImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+      else if (ch === "P") {
+        imageMode(CENTER);
+        image(coopImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+       else if (ch === "W") {
+        imageMode(CENTER);
+        image(wellImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+       else if (ch === "F") {
+        imageMode(CENTER);
+        image(flowerImg, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+      else if (ch === "@") {
+        imageMode(CENTER);
+        image(stonewalk1, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+      else if (ch === "D") {
+        imageMode(CENTER);
+        image(flowerwalk1, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
+      else if (ch === "L") {
+        imageMode(CENTER);
+        image(flowerwalk2, x * tileSize + tileSize/2, y * tileSize + tileSize/2, tileSize, tileSize);
+      } 
       else if (ch === "R") {
         noStroke();//这里开始没有描边
         fill(64, 164, 223);
-        rect(x * tileSize, y * tileSize, tileSize, tileSize);
-      
+        rect(x * tileSize, y * tileSize, tileSize, tileSize,5);
       } 
-      
-    }
+      else if (ch === ">" || ch === "<"|| ch === "^"|| ch === "!"|| ch === "O") {
+        noFill();
+        rect(x * tileSize, y * tileSize, tileSize, tileSize);
+      }
+      }
   }
 }
 
@@ -366,7 +449,7 @@ function handleContinuousMove() {
   if (ny < 0 || ny >= map.length || nx < 0 || nx >= map[0].length) return;
 
   const tile = map[ny][nx];
-  const blocked = (tile === "#" || tile === "T"|| tile === "B"|| tile === "R"|| tile === "1"|| tile === "-"|| tile === "E"|| tile === "X"|| tile === "N"|| tile === "G");//防撞
+  const blocked = (tile === "#" || tile === "T"|| tile === "B"|| tile === "R"|| tile === "1"|| tile === "-"|| tile === "E"|| tile === "X"|| tile === "N"|| tile === "G"|| tile === "A"|| tile === "C"|| tile === "P"|| tile === "W"|| tile === "F");//防撞
   player.dir = newDir;
 
   if (!blocked) {
@@ -422,6 +505,7 @@ function checkTeleport() {
   if (tile === ">") doTeleport(1, 10, 5);
   else if (tile === "<") doTeleport(2, 18, 5);
   else if (tile === "^") doTeleport(3, 10, 8);
+  else if (tile === "!") doTeleport(4, 10, 0);
   else if (tile === "O") doTeleport(0, 10, 5);
 }
 
@@ -447,7 +531,66 @@ function mousePressed() {
       if (interactSound && interactSound.isLoaded()) interactSound.play();
 
       // ✅ 解锁成就
-      unlockAchievement("Shell Acquired!");
+      unlockAchievement("I got a shell!");
+
+      // ✅ （可选）改变地图上该 X 的外观，例如让它消失或变成别的符号
+      const row = map[ty].split("");
+      row[tx] = ".";
+      map[ty] = row.join("");
+    }
+  }
+  // 检测点击的格子是不是 A
+  if (map[ty][tx] === "A") {
+    // 检查玩家是否在 X 的上下左右一格
+    const adjacent =
+      (player.gx === tx && abs(player.gy - ty) === 1) ||
+      (player.gy === ty && abs(player.gx - tx) === 1);
+
+    if (adjacent) {
+      // ✅ 播放音效
+      if (interactSound && interactSound.isLoaded()) interactSound.play();
+
+      // ✅ 解锁成就
+      unlockAchievement("???");
+
+      // ✅ （可选）改变地图上该 X 的外观，例如让它消失或变成别的符号
+      const row = map[ty].split("");
+      row[tx] = ".";
+      map[ty] = row.join("");
+    }
+  }
+
+  // 检测点击的格子是不是 C
+  if (map[ty][tx] === "C") {
+    // 检查玩家是否在 C 的上下左右一格
+    const adjacent =
+      (player.gx === tx && abs(player.gy - ty) === 1) ||
+      (player.gy === ty && abs(player.gx - tx) === 1);
+
+    if (adjacent) {
+      // ✅ 播放音效
+      if (interactSound && interactSound.isLoaded()) interactSound.play();
+
+      // ✅ 解锁成就
+      overlayUntil = millis() + 1000;
+      unlockAchievement("Hero of the Coop");
+  
+      
+    }
+  }
+  // 检测点击的格子是不是 f
+  if (map[ty][tx] === "F") {
+    // 检查玩家是否在 X 的上下左右一格
+    const adjacent =
+      (player.gx === tx && abs(player.gy - ty) === 1) ||
+      (player.gy === ty && abs(player.gx - tx) === 1);
+
+    if (adjacent) {
+      // ✅ 播放音效
+      if (interactSound && interactSound.isLoaded()) interactSound.play();
+
+      // ✅ 解锁成就
+      unlockAchievement("Picked a Tiny Blossom");
 
       // ✅ （可选）改变地图上该 X 的外观，例如让它消失或变成别的符号
       const row = map[ty].split("");
@@ -498,5 +641,19 @@ function mousePressed() {
     }
     
   
+  }
+}
+
+function checkBeside() {
+  if (firstBesideE) return; // 已经触发过则不再执行
+
+  const map = maps[currentMap];
+  const x = player.gx;
+  const y = player.gy;
+
+  // 检查玩家格是否是 E
+  if (x + 1 < map[0].length && map[y][x - 1] === 'E') {
+    unlockAchievement("Sea Gazer");
+    firstBesideE = true; // ✅ 标记已触发
   }
 }
